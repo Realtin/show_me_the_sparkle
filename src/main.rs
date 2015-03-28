33862732a116ab274da085d944cab31e3c35f408
realtin <juliakbln@gmail.com>
2015-03-28 19:10:57 +0100

    prints file contents to the console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::old_io::File;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("list.txt");
+    let mut file = File::open(&path);
+
+    // returns Vector of Bytes (Vec<u8>)
+    let data = file.read_to_end().unwrap();
+
+    // To print the contents of the file
+    let filestr = String::from_utf8(data).unwrap();
+    println!("{}", filestr);
 }
