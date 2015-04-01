c7a43778d549018c9e85c8000daa28e818b1615b
realtin <juliakbln@gmail.com>
2015-04-01 21:12:39 +0200

    get random entry using shuffle & random number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::old_io::File;
+use std::rand::random;
+use std::rand::{thread_rng, Rng};
+
+
+
 
 fn main() {
     let path = Path::new("list.txt");
@@ -10,10 +15,21 @@ fn main() {
     // To print the contents of the file
     let filestr = String::from_utf8(data).unwrap();
 
+    // put every line in an array
     let mut result: Vec<String> = Vec::new();
     for line in filestr.lines_any() {
         result.push(line.to_string());
     }
 
-    println!("{}", result[5]);
+    //get random line from the array
+    let mut rng = thread_rng();
+
+    //using random number
+    let m: usize = rng.gen_range(0, result.len());
+    println!("{}", result[m]);
+
+    //using array shuffle
+    rng.shuffle(&mut result);
+    println!("{}", result[0]);
+
 }
